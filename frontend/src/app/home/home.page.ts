b0976023ea8323aa0101be65fdda7f82c5f0359c
Edu-Estevez-Lemes <eduardoestevez1984@gmail.com>
2025-10-05 22:07:53 +0100

    Frontend Standalone:false

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -4,6 +4,8 @@ import { ClienteService } from '../cliente-service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: false
 })
 export class HomePage implements OnInit {
   constructor(private clientes: ClienteService) {}
